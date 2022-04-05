4c98785b62cd60607c892025fc82405c7d291a48
yitong86 <sophiajiang86@hotmail.com>
2022-04-05 13:24:21 -0400

    CompareTriplets

--- src/com/sophia/Main.java
+++ src/com/sophia/Main.java
@@ -9,7 +9,24 @@ public class Main {
     //staircase
 
     public static void staircase(int n) {
-        // Write your code here
+        // n = 4   numSymbols  numSpace
+        //   #      1             3
+        //  ##      2             2
+        // ###      3             1
+        //####      4             0
+//        for(int i = 0;i < n;i++){
+//            for(int j = 0;j<n;j++){
+//                if(j<n-i-1){
+//                    System.out.print(" ");
+//                }else{
+//                    System.out.print("#");
+//                }
+//            }
+//            System.out.println();
+//        }
+//
+//    }}
+
         for (int numSymbols = 1; numSymbols <= n; numSymbols++) {
             int numSpace = n - numSymbols;
             for (int i = 0; i < numSpace; i++) {
