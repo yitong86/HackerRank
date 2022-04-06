8fa196c8ce6d290dc8df8de8af522e19fc534af3
yitong86 <sophiajiang86@hotmail.com>
2022-04-06 16:32:19 -0400

    update

--- src/com/sophia/BirthdayCakeCandles.java
+++ src/com/sophia/BirthdayCakeCandles.java
@@ -8,13 +8,14 @@ public class BirthdayCakeCandles {
         int higgest = 0;
         int count = 0;
         for(int i = 0 ;i<candles.size();i++){
-            if(candles.get(i) >= higgest){
+            if(candles.get(i) > higgest ){
                 higgest = candles.get(i);
+                count=1;
+            }else if(candles.get(i) == higgest){
                 count+=1;
             }
         }
         return count;
     }
 
-}
 }
