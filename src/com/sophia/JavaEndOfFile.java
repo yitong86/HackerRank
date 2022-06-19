0d84d413842baf93aae1442d0d5e6f9ef94b5efc
yitong86 <sophiajiang86@hotmail.com>
2022-06-19 19:29:11 -0400

    javaendoffile

---
+++ src/com/sophia/JavaEndOfFile.java
@@ -0,0 +1,25 @@
+package com.sophia;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaEndOfFile {
+
+
+
+        public static void main(String[] args) {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner scanner = new Scanner(System.in);
+            int i = 0;
+            while(scanner.hasNext()){
+                i++;
+                System.out.println(i + " " + scanner.nextLine());
+            }
+
+
+        }
+    }
+
+
