255b9e760803bf3e6628228d59680c02256b5b54
yitong86 <sophiajiang86@hotmail.com>
2022-08-05 19:21:36 -0400

    day17

---
+++ src/com/sophia/Day17MoreException.java
@@ -0,0 +1,30 @@
+package com.sophia;
+
+public class Day17MoreException {
+   // class Calculator{
+        int power(int n,int p) throws Exception {
+            if(n>=0 && p>=0) {
+                int result = n;
+                for(int i = 1;i<p;i++){
+                    result = result * n;
+                }
+                return result;
+            }else{
+                throw new Exception("n and p should be non-negative");
+
+            }
+
+        }
+    }
+//class Calculator{
+//    int power(int n,int p) throws Exception {
+//        if(n < 0 || p < 0) {
+//            throw new Exception("n and p should be non-negative");
+//        }else{
+//            int result = (int)Math.pow(n,p);
+//            return result;
+//
+//        }
+//
+//    }
+//}
