faa91a0e3d86d094e250ac78c64ba5e0ff8038b9
yitong86 <sophiajiang86@hotmail.com>
2022-09-19 19:46:31 -0400

    StringToken

--- src/com/sophia/JavaAnagrams.java
+++ src/com/sophia/JavaAnagrams.java
@@ -2,32 +2,33 @@ package com.sophia;
 
 public class JavaAnagrams {
 
-        static boolean isAnagram(String a, String b) {
-            // Complete the function
-            int a_length = a.length();
-            int b_length = b.length();
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        int a_length = a.length();
+        int b_length = b.length();
 
-            if(a_length != b_length) return false;
+        if (a_length != b_length) return false;
 
-            a = a.toLowerCase();
-            b = b.toLowerCase();
+        a = a.toLowerCase();
+        b = b.toLowerCase();
 
-            int[] char_frequencies = new int[26];
+        int[] char_frequencies = new int[26];
 
-            for(int i =0; i< a .length();i++){
-                char current_char = a.charAt(i);
-                int index = current_char-'a';
-                char_frequencies[index]++;
-            }
-            for(int i =0; i< b .length();i++){
-                char current_char = b.charAt(i);
-                int index = current_char-'a';
-                char_frequencies[index]--;
-            }
-            for(int i = 0;i<26;i++){
-                if(char_frequencies[i] != 0)return false;
-
-            }
-            return true;
+        for (int i = 0; i < a.length(); i++) {
+            char current_char = a.charAt(i);
+            int index = current_char - 'a';
+            char_frequencies[index]++;
         }
+        for (int i = 0; i < b.length(); i++) {
+            char current_char = b.charAt(i);
+            int index = current_char - 'a';
+            char_frequencies[index]--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (char_frequencies[i] != 0) return false;
+
+        }
+        return true;
+    }
+}
 
