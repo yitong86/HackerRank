610aee91cc3491b7cd07847f3d1abf95544891aa
yitong86 <sophiajiang86@hotmail.com>
2022-09-21 16:29:20 -0400

    PatternSyntaxChecker

---
+++ src/com/sophia/java/PatternSyntaxChexker.java
@@ -0,0 +1,28 @@
+package com.sophia.java;
+import java.util.Scanner;
+import java.util.regex.*;
+
+
+public class PatternSyntaxChexker {
+
+        public static void main(String[] args){
+            Scanner in = new Scanner(System.in);
+            int testCases = Integer.parseInt(in.nextLine());
+            while(testCases>0){
+                String pattern = in.nextLine();
+                //Write your code
+                try{
+                    Pattern.compile(pattern);
+                    System.out.println("Valid");
+                }catch(Exception e ){
+                    System.out.println("Invalid");
+                }
+                testCases--;
+            }
+        }
+    }
+
+
+
+
+
