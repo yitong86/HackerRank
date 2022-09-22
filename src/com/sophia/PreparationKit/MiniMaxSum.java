db720216ffac6c3ea3b950352e804528d4f33c47
yitong86 <sophiajiang86@hotmail.com>
2022-09-22 09:31:44 -0400

    MinMaxSum

---
+++ src/com/sophia/PreparationKit/MiniMaxSum.java
@@ -0,0 +1,36 @@
+package com.sophia.PreparationKit;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+public class MiniMaxSum {
+
+        /*
+         * Complete the 'miniMaxSum' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static void miniMaxSum(List<Integer> arr) {
+            int min = arr.get(0);
+            int max = arr.get(0);
+            long sum = 0;
+            for(int i = 0;i<arr.size();i++){
+                sum +=arr.get(i);
+                if(arr.get(i)<min){
+                    min = arr.get(i);
+                }
+                if(arr.get(i)>max){
+                    max = arr.get(i);
+                }
+            }
+            System.out.print(sum-max + " ");
+            System.out.print(sum-min);
+
+        }
+
+    }
+
