4dd6c127c9d29ae6112963b5b2b9bac76c50ffb9
yitong86 <sophiajiang86@hotmail.com>
2022-10-08 19:41:15 -0400

    addComments

--- src/com/sophia/java/JavaGeneric.java
+++ src/com/sophia/java/JavaGeneric.java
@@ -6,8 +6,8 @@ import java.lang.reflect.Method;
 public class JavaGeneric {
 
 
-    class Printer {
-        <T> void printArray(T[] array) {
+    class Printer<T> {
+        public <T> void printArray(T[] array) {
             for (T element : array) {
                 System.out.println(element);
             }
@@ -16,11 +16,23 @@ public class JavaGeneric {
 
     public class Solution {
         public void main(String args[]) {
-            Printer myPrinter = new Printer();
+            Printer<Integer> myPrinter = new Printer();
+            Printer<String> myPrinter1 = new Printer();
+
             Integer[] intArray = {1, 2, 3};
-            String[] stringArray = {"Hello", "World"};
+            String[] stringArray1 = {"Hello", "World"};
             myPrinter.printArray(intArray);
-            myPrinter.printArray(stringArray);
+            myPrinter1.printArray(stringArray1);
+
+
+
+
+           // Printer myPrinter = new Printer();
+            // //Integer[] intArray = { 1, 2, 3 };
+            // String[] stringArray = { "Hello", "World" };
+            // myPrinter.printArray(intArray);
+            // myPrinter.printArray(stringArray);
+
             int count = 0;
             for (Method method : Printer.class.getDeclaredMethods()) {
                 String name = method.getName();
