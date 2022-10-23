6f571fd919435275a5ab57ac9d16073166e07544
yitong86 <sophiajiang86@hotmail.com>
2022-10-23 19:13:45 -0400

    RegularExpression

---
+++ src/com/sophia/java/ValidUsernameRegularEx.java
@@ -0,0 +1,10 @@
+package com.sophia.java;
+import java.util.Scanner;
+
+public class ValidUsernameRegularEx {
+
+   // class UsernameValidator {
+
+        public static final String regularExpression= "^[a-zA-Z]\\w{7,29}";
+    }
+
