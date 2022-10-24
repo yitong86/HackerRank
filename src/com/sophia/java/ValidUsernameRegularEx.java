14f56cbd28c53b032c0b63ef6166774318034959
yitong86 <sophiajiang86@hotmail.com>
2022-10-24 18:52:32 -0400

    Iterator

--- src/com/sophia/java/ValidUsernameRegularEx.java
+++ src/com/sophia/java/ValidUsernameRegularEx.java
@@ -8,3 +8,7 @@ public class ValidUsernameRegularEx {
         public static final String regularExpression= "^[a-zA-Z]\\w{7,29}";
     }
 
+//    A valid username should start with an alphabet so, [A-Za-z].
+//        All other characters can be alphabets, numbers or an underscore so, [A-Za-z0-9_].
+//        Since length constraint was given as 8-30 and we had already fixed the first character, so we give {7,29}.
+//        We use ^ and $ to specify the beginning and end of matching.
