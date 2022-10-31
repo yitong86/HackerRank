b0f8ed0b141a2d73b543b7b403421144d9e7b0e0
yitong86 <sophiajiang86@hotmail.com>
2022-10-31 17:13:27 -0400

    addComments

--- src/com/sophia/PreparationKit/NumberLineJumps.java
+++ src/com/sophia/PreparationKit/NumberLineJumps.java
@@ -12,4 +12,12 @@ public class NumberLineJumps {
     }
 
 }
-
+//j= jump
+//v=d/t=d/j
+//d=vj
+//x1+v1*j = x2+ v2*j
+//v1j-v2j = x2-x1
+//j(v1-v2) = x2-x1
+//j = x2-x1/v1-v2
+//r= x2-x1 % v1-v2 = 0
+//
