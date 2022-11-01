7f7ebe075a44b990aa4bfd0f2c6539bc49c7350e
yitong86 <sophiajiang86@hotmail.com>
2022-11-01 18:45:44 -0400

    override

--- src/com/sophia/PreparationKit/SeparateNumbers.java
+++ src/com/sophia/PreparationKit/SeparateNumbers.java
@@ -2,15 +2,15 @@ package com.sophia.PreparationKit;
 
 public class SeparateNumbers {
 
-    public static void separateNumbers(String s) {
+    public static void separateNumbers(String s) {//91011
         String subString = "";
         boolean isBeautiful = false;
-        for(int i =1;i<=s.length()/2;i++){
-            subString = s.substring(0,i);
+        for(int i =1;i<=s.length()/2;i++){//check every possible substring(number) of different length,up to the mid length of the inout string
+            subString = s.substring(0,i);//how many digital number
             Long num = Long.parseLong(subString);
             String  beautifulString = subString;
             while(beautifulString.length()<s.length()){
-                beautifulString +=Long.toString(++num);
+                beautifulString +=Long.toString(++num);//9 910 911
             }
             if(s.equals(beautifulString)){
                 isBeautiful = true;
---
+++ src/com/sophia/java/OverridingMethod.java
@@ -0,0 +1,9 @@
+package com.sophia.java;
+
+public class OverridingMethod {
+    @Override
+    public void getNumberOfTeamMembers(){
+        //System.out.println( "Each team has 11 players in " + getName());
+    }
+
+}
