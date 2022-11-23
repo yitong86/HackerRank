77e68e36ca1655b468f9f9eb4e716663c5086413
yitong86 <sophiajiang86@hotmail.com>
2022-11-22 21:11:36 -0500

    addDifferentSolution

--- src/com/sophia/java/JavaVarargs.java
+++ src/com/sophia/java/JavaVarargs.java
@@ -1,17 +1,38 @@
 package com.sophia.java;
 
 public class JavaVarargs {
+    //    class Add {
+//        void add(int... numbers) {
+//            StringBuffer sb = new StringBuffer();
+//            int sum = 0;
+//            for (int num : numbers) {
+//                sb.append(num + "+");
+//                sum += num;
+//            }
+//            sb.setCharAt(sb.length() - 1, '=');
+//            sb.append(sum);
+//            System.out.println(sb);
+//        }
+//    }
+//}
     class Add {
-        void add(int... numbers) {
-            StringBuffer sb = new StringBuffer();
+        public void add(int... arr) {
+
             int sum = 0;
-            for (int num : numbers) {
-                sb.append(num + "+");
-                sum += num;
+            for (int i = 0; i < arr.length; i++) {
+                if (i == arr.length - 1) {
+                    System.out.print(arr[i] + "=");
+                } else {
+
+                    System.out.print(arr[i] + "+");
+
+                }
+                sum = sum + arr[i];
+
             }
-            sb.setCharAt(sb.length() - 1, '=');
-            sb.append(sum);
-            System.out.println(sb);
+            System.out.print(sum);
+            System.out.println();
+
         }
     }
 }
