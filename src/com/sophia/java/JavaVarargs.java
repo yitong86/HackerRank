ffd2292070fde249fcd7b3a4ae8be6fdbc3ab702
yitong86 <sophiajiang86@hotmail.com>
2022-11-22 20:38:33 -0500

    javaVarargs

---
+++ src/com/sophia/java/JavaVarargs.java
@@ -0,0 +1,17 @@
+package com.sophia.java;
+
+public class JavaVarargs {
+    class Add {
+        void add(int... numbers) {
+            StringBuffer sb = new StringBuffer();
+            int sum = 0;
+            for (int num : numbers) {
+                sb.append(num + "+");
+                sum += num;
+            }
+            sb.setCharAt(sb.length() - 1, '=');
+            sb.append(sum);
+            System.out.println(sb);
+        }
+    }
+}
