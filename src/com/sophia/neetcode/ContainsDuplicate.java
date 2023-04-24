dc440b9578cfd350115c0186de1ad41b1d8dda61
yitong86 <sophiajiang86@hotmail.com>
2023-04-24 14:51:27 -0400

    containsDuplicate

---
+++ src/com/sophia/neetcode/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+package com.sophia.neetcode;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+
+   // class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            Arrays.sort(nums);
+            for(int i = 0;i<nums.length-1;i++){
+
+                if(nums[i] == nums[i+1]){
+
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+
