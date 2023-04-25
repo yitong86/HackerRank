2b5eb2fa5e39274a24b798e5e22ab770e2a2ac0e
yitong86 <sophiajiang86@hotmail.com>
2023-04-25 16:47:50 -0400

    addMethod

--- src/com/sophia/neetcode/ValidAnagram.java
+++ src/com/sophia/neetcode/ValidAnagram.java
@@ -1,20 +1,37 @@
 package com.sophia.neetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ValidAnagram {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
-            int[] charCount = new int[26];
-            for (int i = 0; i < s.length(); i++) {
-                charCount[s.charAt(i) - 'a']++;
-                charCount[t.charAt(i) - 'a']--;
+
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+        }
+        for (char c : map.keySet()) {
+            if (map.get(c) != 0) {
+                return false;
             }
-            for (int count : charCount) {
-                if (count != 0) {
-                    return false;
-                }
-            }
-            return true;
+        }
+        return true;
+
+//        if (s.length() != t.length())
+//            return false;
+//            int[] charCount = new int[26];
+//            for (int i = 0; i < s.length(); i++) {
+//                charCount[s.charAt(i) - 'a']++;
+//                charCount[t.charAt(i) - 'a']--;
+//            }
+//            for (int count : charCount) {
+//                if (count != 0) {
+//                    return false;
+//                }
+//            }
+//            return true;
 
 
     }
