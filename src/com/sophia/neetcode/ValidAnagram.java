748e85d54803d9ef9b52fd482472b52f816a1e2a
yitong86 <sophiajiang86@hotmail.com>
2023-04-25 16:31:39 -0400

    validAnagram

---
+++ src/com/sophia/neetcode/ValidAnagram.java
@@ -0,0 +1,21 @@
+package com.sophia.neetcode;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+            int[] charCount = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                charCount[s.charAt(i) - 'a']++;
+                charCount[t.charAt(i) - 'a']--;
+            }
+            for (int count : charCount) {
+                if (count != 0) {
+                    return false;
+                }
+            }
+            return true;
+
+
+    }
+}
