e2969d546444b639038c0bacc0a28ff0b7c9721d
yitong86 <sophiajiang86@hotmail.com>
2023-04-29 13:26:08 -0400

    updateMinMaxSum

--- src/com/sophia/PreparationKit/MiniMaxSum.java
+++ src/com/sophia/PreparationKit/MiniMaxSum.java
@@ -15,22 +15,37 @@ public class MiniMaxSum {
          */
 
         public static void miniMaxSum(List<Integer> arr) {
-            int min = arr.get(0);
-            int max = arr.get(0);
-            long sum = 0;
-            for(int i = 0;i<arr.size();i++){
-                sum +=arr.get(i);
-                if(arr.get(i)<min){
-                    min = arr.get(i);
+                Collections.sort(arr);
+                int minSum = 0;
+                int maxSum = 0;
+                int sum = 0;
+                for(int i = 0;i<arr.size();i++){
+                    sum += arr.get(i);
+                    minSum = sum - arr.get(arr.size()-1);
+                    maxSum = sum - arr.get(0);
                 }
-                if(arr.get(i)>max){
-                    max = arr.get(i);
-                }
+                System.out.println(minSum + " " + maxSum);
+
             }
-            System.out.print(sum-max + " ");
-            System.out.print(sum-min);
 
         }
 
-    }
+//            int min = arr.get(0);
+//            int max = arr.get(0);
+//            long sum = 0;
+//            for(int i = 0;i<arr.size();i++){
+//                sum +=arr.get(i);
+//                if(arr.get(i)<min){
+//                    min = arr.get(i);
+//                }
+//                if(arr.get(i)>max){
+//                    max = arr.get(i);
+//                }
+//            }
+//            System.out.print(sum-max + " ");
+//            System.out.print(sum-min);
+//
+//        }
+
+
 
