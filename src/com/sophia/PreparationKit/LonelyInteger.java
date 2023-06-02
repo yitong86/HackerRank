2748241a7ef7c1c0f5823cc724bb21d9e751b454
yitong86 <sophiajiang86@hotmail.com>
2023-06-02 16:54:19 -0400

    longlyNumber

--- src/com/sophia/PreparationKit/LonelyInteger.java
+++ src/com/sophia/PreparationKit/LonelyInteger.java
@@ -19,18 +19,10 @@ import java.util.regex .*;
 public class LonelyInteger {
 
 
-
-        /*
-         * Complete the 'lonelyinteger' function below.
-         *
-         * The function is expected to return an INTEGER.
-         * The function accepts INTEGER_ARRAY a as parameter.
-         */
-
     public static int lonelyinteger(List<Integer> a) {
         // Write your code here
         Set<Integer> set = new HashSet<>();
-        int number = 0;
+        int Lonelynumber = 0;
         for(Integer i :a){
             if(!set.contains(i)){
                 set.add(i);
@@ -39,36 +31,11 @@ public class LonelyInteger {
             }
         }
         for(Integer s :set){
-            number = s;
+            Lonelynumber = s;
         }
-        return number;
+        return Lonelynumber;
     }
 
 }
-
-
-//
-//
-//        /*
-//         * Complete the 'lonelyinteger' function below.
-//         *
-//         * The function is expected to return an INTEGER.
-//         * The function accepts INTEGER_ARRAY a as parameter.
-//         */
-//
-//        public static int lonelyinteger(List<Integer> a) {
-//            // Write your code here
-//            int number=a.get(0);
-//            for(int i=1;i<a.size();i++){
-//                number ^=a.get(i);
-//            }
-//            return number;
-//        }
-//
-//    }
-
-
-
-
 
 
